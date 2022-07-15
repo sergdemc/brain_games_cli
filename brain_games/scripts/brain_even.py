2e909165f20bed9bec73f5fc6afdd75b872fcac7
Sergei Demchenko <hb13718@gmail.com>
2022-07-15 17:11:39 +0300

    Add brain_even.py

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from brain_games.game_logic.brain_even_logic import greet_user
+from brain_games.game_logic.brain_even_logic import start_brain_even
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    greet_user()
+    start_brain_even()
+
+
+if __name__ == '__main__':
+    main()
