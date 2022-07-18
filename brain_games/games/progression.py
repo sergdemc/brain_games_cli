a5c5c806480296109f182ef50402eb0753c415a6
Sergei Demchenko <hb13718@gmail.com>
2022-07-18 15:43:10 +0300

    Add brain-progression game

---
+++ brain_games/games/progression.py
@@ -0,0 +1,29 @@
+import random
+import prompt
+
+
+def get_question():
+    print('What number is missing in the progression?')
+
+
+def get_answer():
+    progression = generate_progression()
+    missing_index = random.randint(0, len(progression) - 1)
+    missing_num = progression[missing_index]
+    progression[missing_index] = '..'
+    print('Question: ', *progression)
+    user_answer = prompt.string('Your answer: ')
+    right_answer = missing_num
+    return right_answer, user_answer
+
+
+def generate_progression() -> list:
+    start_progression = random.randint(0, 20)
+    step_progression = random.randint(2, 20)
+    len_progression = random.randint(8, 12)
+    progression = [start_progression]
+    count = 1
+    while count <= len_progression:
+        progression.append(progression[-1] + step_progression)
+        count += 1
+    return progression
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""Brain progression game"""
+
+from brain_games.games.progression import get_question, get_answer
+from brain_games.engine import run_game
+
+
+def main():
+    return run_game(get_question, get_answer)
+
+
+if __name__ == '__main__':
+    main()
